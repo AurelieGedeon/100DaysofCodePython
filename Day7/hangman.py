ebb08cda122200b424e3b5b8a22de915655fdf44
Aurelie Gedeon <agedeon540@gmail.com>
2024-03-19 17:05:12 -0500

    Made a function for hangman; added input to allow user to decide if they want to play again

--- Day7/hangman.py
+++ Day7/hangman.py
@@ -2,40 +2,57 @@ import random
 from word_list import *
 from hangman_art import *
 
+def play_hangman():
+    chosen_word = random.choice(word_list)
 
-chosen_word = random.choice(word_list)
+    num_of_letters= len(chosen_word)
 
-num_of_letters= len(chosen_word)
+    display = ['_'] * num_of_letters
+    wrong_guesses = []
+    all_guesses = []
 
-display = ['_'] * num_of_letters
-wrong_guesses = []
+    game_over = False
 
-game_over = False
+    lives = 6
 
-lives = 6
+    print(logo)
 
-print(logo)
+    while game_over == False:
+        guess = input('Guess a letter: ').lower()
 
-while game_over == False:
-    guess = input('Guess a letter: ').lower()
+        if guess in all_guesses:
+            print("You already chose this letter; chose another one.")
+            continue
 
-    for position in range(num_of_letters):
-        letter = chosen_word[position]
-        if letter == guess:
-            display[position] = letter
-    if guess not in chosen_word:
-        lives -= 1
-        wrong_guesses.append(guess)
-    
-    print(stages[lives])
-    print(f'Incorrect guesses: {wrong_guesses}')
-    print(f' '.join(display))
+        if guess in chosen_word:
+            for position in range(num_of_letters):
+                letter = chosen_word[position]
+                if letter == guess:
+                    display[position] = letter
+        else:
+            if guess not in all_guesses:
+                lives -= 1
+                wrong_guesses.append(guess)
 
-    if lives == 0:
-        game_over = True
-        print("You lose!")
-        print(f'The correct word was: {chosen_word}') 
-    elif '_' not in display:
-        game_over = True
-        print("You win!")
-    
+        all_guesses.append(guess)
+        
+        print(stages[lives])
+        print(f'All Guesses: {all_guesses}')
+        print(f'Incorrect guesses: {wrong_guesses}')
+        print(f' '.join(display))
+
+        if lives == 0:
+            game_over = True
+            print("You lose!")
+            print(f'The correct word was: {chosen_word}') 
+        elif '_' not in display:
+            game_over = True
+            print("You win!")
+    play_again = input("Would you like again? (y/n): ").lower()
+
+    if play_again == 'y':
+        play_hangman()
+    else:
+        print("Thank you for playing!")
+
+play_hangman()
