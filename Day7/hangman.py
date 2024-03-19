8d5b71100d19a6fa1a6c706dc5f4dd38601d54a0
Aurelie Gedeon <agedeon540@gmail.com>
2024-03-19 15:31:22 -0500

    Added display array to show blank spots for letters of word

--- Day7/hangman.py
+++ Day7/hangman.py
@@ -4,6 +4,10 @@ word_list = ['aardvark', 'baboon', 'camel']
 
 chosen_word = random.choice(word_list)
 
+num_of_letters = len(chosen_word)
+display = ['_'] * num_of_letters
+print(display)
+
 print(chosen_word)
 
 guess = input('Guess a letter: ').lower()
@@ -11,4 +15,5 @@ guess = input('Guess a letter: ').lower()
 for letter in chosen_word:
     if letter == guess:
         print("huzzah!")
-    
+    else:
+        print('wrong!!!')
