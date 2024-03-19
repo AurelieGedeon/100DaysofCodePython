689b3c3bc4ec2d61f70b3122527d74f952260f03
Aurelie Gedeon <agedeon540@gmail.com>
2024-03-19 15:24:59 -0500

    Added conditional for user guess

--- Day7/hangman.py
+++ Day7/hangman.py
@@ -7,3 +7,9 @@ chosen_word = random.choice(word_list)
 print(chosen_word)
 
 guess = input('Guess a letter: ').lower()
+
+chosen_word_letters = [*chosen_word]
+print(chosen_word_letters)
+
+if guess in chosen_word_letters:
+    print("huzzah!")
