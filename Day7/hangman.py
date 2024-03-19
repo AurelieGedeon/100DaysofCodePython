23018486098cf33f934871c022040303fbcad19b
Aurelie Gedeon <agedeon540@gmail.com>
2024-03-19 15:26:13 -0500

    Removed excess code

--- Day7/hangman.py
+++ Day7/hangman.py
@@ -8,8 +8,6 @@ print(chosen_word)
 
 guess = input('Guess a letter: ').lower()
 
-chosen_word_letters = [*chosen_word]
-print(chosen_word_letters)
 
-if guess in chosen_word_letters:
+if guess in chosen_word:
     print("huzzah!")
