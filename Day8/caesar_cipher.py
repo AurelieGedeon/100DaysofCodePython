7a2961117fb032d32e1ebfa66b19aa252cd54125
Aurelie Gedeon <agedeon540@gmail.com>
2024-03-20 22:26:17 -0500

    Set up encrypt function to take in text and output new encrypted message based on shift number amount

--- Day8/caesar_cipher.py
+++ Day8/caesar_cipher.py
@@ -4,3 +4,14 @@ direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
+def encrypt(plain_text, shift_amount):
+    encrypted_message = ""
+    for letter in plain_text:
+        letter_index = alphabet.index(letter)
+        encrypted_index =  letter_index + shift_amount
+        encrypted_letter = alphabet[encrypted_index]
+        encrypted_message += encrypted_letter
+    print(encrypted_message)
+
+
+encrypt(text, shift)
