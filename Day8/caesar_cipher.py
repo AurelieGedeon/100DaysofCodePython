38429b574885e9e5b272c04c6d13c8d6beda062a
Aurelie Gedeon <agedeon540@gmail.com>
2024-03-20 22:48:23 -0500

    Moved variable outside of scope for future decrypting function

--- Day8/caesar_cipher.py
+++ Day8/caesar_cipher.py
@@ -3,10 +3,10 @@ alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n'
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
+alpha_index = len(alphabet) - 1
 
 def encrypt(plain_text, shift_amount):
     encrypted_message = ""
-    alpha_index = len(alphabet) - 1
     for letter in plain_text:
         letter_index = alphabet.index(letter)
         encrypted_index =  letter_index + shift_amount
