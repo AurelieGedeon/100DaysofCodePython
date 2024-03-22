2c2619f28b2a9d6a5966124360231a7b6cccb991
Aurelie Gedeon <agedeon540@gmail.com>
2024-03-21 23:15:52 -0500

    Added while loop to keep running through cipher until user ends it

--- Day8/caesar_cipher.py
+++ Day8/caesar_cipher.py
@@ -2,6 +2,7 @@ from cipher_art import logo
 
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 alpha_length = len(alphabet)
+cipher_over = False
 
 def caesar(start_text, shift_amount, cipher_direction):
     end_text = ""
@@ -16,13 +17,20 @@ def caesar(start_text, shift_amount, cipher_direction):
             new_letter = alphabet[shifted_index]
             end_text += new_letter
     print(f"The {direction}d message is {end_text}")
+while cipher_over == False:
+    print(logo)
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+    caesar(text, shift, direction)
+    end_cipher = input("Type 'yes' if you want to go again. Otherwise type 'no'. ").lower()
 
-print(logo)
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
+    if end_cipher == "no":
+        cipher_over = True
+    elif end_cipher == "yes":
+        cipher_over = False
 
-caesar(text, shift, direction)
+
 
 
 
