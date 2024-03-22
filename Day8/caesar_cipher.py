4ce7fceaf62fdf74412c117fde285a665f829def
Aurelie Gedeon <agedeon540@gmail.com>
2024-03-21 22:47:39 -0500

    Reordered function and inputs

--- Day8/caesar_cipher.py
+++ Day8/caesar_cipher.py
@@ -1,11 +1,6 @@
 from cipher_art import logo
 
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-
-print(logo)
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
 alpha_length = len(alphabet)
 
 def caesar(start_text, shift_amount, cipher_direction):
@@ -22,7 +17,14 @@ def caesar(start_text, shift_amount, cipher_direction):
             end_text += new_letter
     print(f"The {direction}d message is {end_text}")
 
+print(logo)
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
+
 caesar(text, shift, direction)
+
+
 
 
 
