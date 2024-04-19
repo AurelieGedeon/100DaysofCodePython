83c9128375eafcf4308b2337ea57c42749ee8009
Aurelie Gedeon <agedeon540@gmail.com>
2024-04-18 22:24:43 -0500

    Consolidated function into one instead of four

--- Day10/calculator.py
+++ Day10/calculator.py
@@ -11,3 +11,16 @@ def multiply(n1, n2):
 
 def divide(n1, n2):
     return n1 / n2
+
+operations = {
+    '+' : add,
+    '-' : subtract,
+    '*' : multiply,
+    '/' : divide
+}
+
+def calculate(n1, n2, operation):
+    problem = f"{n1} {operation} {n2}"
+    return eval(problem)
+
+print(calculate(7,3,'*'))
